e8bcdc0a265c3ca3ec2c1fa685c6a8bf7a8783be
kessler <yaron.kessler41@gmail.com>
2019-02-04 17:41:11 +0200

    ubuntu setting

---
+++ ubuntu/install-with-script/more-settings.sh
@@ -0,0 +1,10 @@
+
+# change language keys, more setting
+sudo apt-get install gnome-tweaks
+
+gnome-tweaks
+
+# Select Keyboard & Mouse tab
+# Click Additional Layout Options button
+# Expand Switching to another layout
+# Select [Ctrl] + [Shift] here
