94e6a23b41bacd7d3c3834fd1b378c1c242f3df1
koskas <koskas@yahoo.com>
2019-02-17 23:28:21 +0200

    test'

---
+++ ubuntu/install-with-script/angular.sh
@@ -0,0 +1,12 @@
+# 
+# Angular install
+# 
+
+sudo apt-get update
+
+# nodeJs version 11.x 
+curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
+
+sudo apt-get install -y nodejs
+
+sudo apt-get update
