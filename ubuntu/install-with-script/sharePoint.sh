50bd074e9219fd51165b3a725afad6cda37757fb
koskas <koskas@yahoo.com>
2019-03-08 09:53:34 +0200

    test

--- ubuntu/install-with-script/sharePoint.sh
+++ ubuntu/install-with-script/sharePoint.sh
@@ -2,13 +2,14 @@
 sudo groupadd -g 1100 kessler
 
 ## add user to group (user --> group)
-sudo adduser user kessler
+sudo adduser jaron kessler
+sudo adduser yaron kessler
 sudo adduser ofir kessler
 sudo adduser noam kessler
- sudo adduser suziekessler
+sudo adduser suzie kessler
 
 ## set group directory/folder owner
-sudo chown -Rv user:kessler /srv
+sudo chown -Rv yaron:kessler /srv
 
 ## sest directory/folder permission
 sudo chmod -Rv 774 /srv
