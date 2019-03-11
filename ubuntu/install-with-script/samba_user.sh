d5f395b0cc08901be68a0ef57a7de38d68258869
koskas <koskas@yahoo.com>
2019-03-11 11:20:13 +0200

    share point script

---
+++ ubuntu/install-with-script/samba_user.sh
@@ -0,0 +1,41 @@
+#samba user
+
+
+##install samba
+sudo apt-get install samba* -y
+
+## samba user to samaba + set password
+smbpasswd -a user
+smbpasswd -a noam
+smbpasswd -a ofir
+smbpasswd -a suzie
+
+##set shared folder
+sudo vim /etc/samba/smb.conf
+
+
+[srv]
+   comment = share point
+   path = /srv
+   guest ok = no
+   browseable = yes
+   create mask = 0600
+   directory mask = 0700
+   valid users=user noam ofir suzie
+   public = no
+   writeable = yes
+
+
+[printers]
+	comment = All Printers
+	path = /var/spool/samba
+	create mask = 0700
+	printable = Yes
+	browseable = No
+
+## restart service in two/2 ways
+#1#
+## service smb restart
+
+#2#
+## /etc/init.d/smb reload
---
+++ ubuntu/install-with-script/update_os.sh
@@ -0,0 +1,15 @@
+#
+# os update - script #
+#
+
+##--[Begin]--##
+
+
+# #1 --[update]--
+sudo apt-get update -y
+
+# #2 --[upgrade]--
+sudo apt-get upgrade -y
+
+
+##--[End]--##
