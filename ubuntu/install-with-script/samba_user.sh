caf382b96aec63c3beeb8fec1ee144615470349b
koskas <koskas@yahoo.com>
2019-03-11 12:16:13 +0200

    test

--- ubuntu/install-with-script/samba_user.sh
+++ ubuntu/install-with-script/samba_user.sh
@@ -1,41 +1,96 @@
-#samba user
+#
+# samba - script #
+#
 
 
-##install samba
-sudo apt-get install samba* -y
-
-## samba user to samaba + set password
-smbpasswd -a user
-smbpasswd -a noam
-smbpasswd -a ofir
-smbpasswd -a suzie
-
-##set shared folder
-sudo vim /etc/samba/smb.conf
+##--[Begin]--##
 
 
-[srv]
-   comment = share point
-   path = /srv
-   guest ok = no
-   browseable = yes
-   create mask = 0600
-   directory mask = 0700
-   valid users=user noam ofir suzie
-   public = no
-   writeable = yes
+# #1 --[update os]-- #
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+# #2 --[install samba]-- #
+sudo apt-get install samba -y
+
+# #2 --[samba - check if samba install exists]
+echo ".."
+echo "Attention: where is samba:..... "
+echo ".."
+whereis samba
+
+# #3 --[ubuntu user into samba]-- #
+## set up ubuntu user to samaba user accout (set password)
+# echo "smb password change --> jaron"
+# sudo smbpasswd -a jaron
+
+# echo "smb password change --> yaron"
+# sudo smbpasswd -a yaron
+
+# echo "smb password change --> ofir"
+# sudo smbpasswd -a ofir
+
+# echo "smb password change --> noam"
+# sudo smbpasswd -a noam
+
+# echo "smb password change --> suzie"
+# sudo smbpasswd -a suzie
+
+# #4 --[samba share point ]-- #
+## create share point director/folder
+## <sharePoint.sh>
+
+# #5 --[set shared point (folder)]-- #
+## set share point (folder) into "/etc/samba/smb.conf" config file.
+#sudo vim /etc/samba/smb.conf
+
+# #6 --[set shared point (folder)]-- #
+echo "##"
+echo "##"
+echo "## copy and past this source into /etc/samba/smb.conf end of file "
+echo "##"
+echo "##source - begin##"
+echo "##"
+echo "[srv]"
+echo "comment = share point"
+echo "path = /srv"
+echo "guest ok = no"
+echo "browseable = yes"
+echo "read only = no"
+echo "create mask = 0600"
+echo "directory mask = 0700"
+echo "valid users=jaron yaron noam ofir suzie kessler"
+echo "public = no"
+echo "writeable = yes"
+
+#echo "## printers share - if printer exists##
+#echo "[printers]
+#echo "comment = All Printers
+#echo "path = /var/spool/samba
+#echo "create mask = 0700
+#echo "printable = Yes
+#echo "browseable = No
+
+echo "##"
+echo "##source - end ##"
 
 
-[printers]
-	comment = All Printers
-	path = /var/spool/samba
-	create mask = 0700
-	printable = Yes
-	browseable = No
-
-## restart service in two/2 ways
+# #7 --[restart service in two/3 ways]-- #
+## restart service in two/3 ways
 #1#
-## service smb restart
+echo "##"
+echo "##"
+echo "## restart samba service ## "
+echo "##"
+echo "sudo service smbd restart"
+echo "##"
+sudo service smbd restart
 
-#2#
-## /etc/init.d/smb reload
+#2# 
+###sudo service smb restart
+
+#3#
+###sudo /etc/init.d/smb reload
+
+
+##--[End]--##
