e1eac74deb220c5da4d1eee09fcb63aa769c15b9
yaron <koskas@yahoo.com>
2019-04-10 21:20:25 +0300

    jdk 11 + 12

--- ubuntu/install-with-script/java_jdk_jre_install.sh
+++ ubuntu/install-with-script/java_jdk_jre_install.sh
@@ -1,55 +1,63 @@
 #### install java - jdk / jre ####
 
 
-##update the system##
+## update the system##
 sudo apt-get update
 
 sudo apt-get upgrade
 
-##Java-jre##
+## Java-jre##
 sudo apt-get install default-jre -y
-##Java-jdk##
+## Java-jdk##
 sudo apt-get install default-jdk -y
 
 
 
-##Java-open-jre7##
-##sudo apt-get install openjdk-7-jre -y
-##Java-open-jdk7##
-##sudo apt-get install openjdk-7-jdk -y
+## Java-open-jre7##
+## sudo apt-get install openjdk-7-jre -y
+## Java-open-jdk7##
+## sudo apt-get install openjdk-7-jdk -y
 
-##Java-open-jre8##
-##sudo apt-get install openjdk-8-jre -y
-##Java-open-jdk8##
-##sudo apt-get install openjdk-8-jdk -y
+## Java-open-jre8##
+## sudo apt-get install openjdk-8-jre -y
+## Java-open-jdk8##
+## sudo apt-get install openjdk-8-jdk -y
 
-##Java-open-jre9##
+## Java-open-jre9##
 sudo apt-get install openjdk-9-jre -y
-##Java-open-jdk9##
+## Java-open-jdk9##
 sudo apt-get install openjdk-9-jdk -y
 
 
 
-##oracle - old installation##
+## oracle - old installation##
 sudo apt-get install python-software-properties -y
 sudo add-apt-repository ppa:webupd8team/java -y
 sudo apt-get update -y
 
 
-##oracle jdk-6##
-##sudo apt-get install oracle-java6-installer -y
+## oracle jdk-6 ##
+## sudo apt-get install oracle-java6-installer -y
 
 
-##oracle jdk-6##
-##sudo apt-get install oracle-java7-installer -y
+## oracle jdk-6##
+## sudo apt-get install oracle-java7-installer -y
 
 
-##oracle jdk-8##
+## oracle jdk-8 ##
 sudo apt-get install oracle-java8-installer -y
 
 
-##oracle jdk-9##
+## oracle jdk-9 ##
 sudo apt-get install oracle-java9-installer -y
+
+
+## oracle jdk-11 / 12 ##
+sudo add-apt-repository ppa:linuxuprising/java
+sudo apt-get update
+#sudo apt-get install oracle-java10-installer
+sudo apt-get install oracle-java11-installer -y
+sudo apt-get install oracle-java12-installer -y
 
 
 ##Managing Java##
