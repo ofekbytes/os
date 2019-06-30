3291146b1ef025b4066783f00fc95a3176e94946
koskas <koskas@yahoo.com>
2019-06-30 12:57:13 +0300

    test

---
+++ ubuntu/install-with-script/ApacheGUI-start-service.sh
@@ -0,0 +1,18 @@
+###
+# ApacheGUI-start-service
+###
+
+#
+## update the system ##
+#sudo apt-get update
+#sudo apt-get upgrade
+
+## start service
+cd /usr/local/apachegui/ApacheGUI/bin
+./run.sh
+
+echo " .... "
+echo " .... "
+echo " .... Service Started .... "
+echo " .... "
+echo " .... "
---
+++ ubuntu/install-with-script/ApacheGUI-stop-service.sh
@@ -0,0 +1,18 @@
+#
+# ApacheGUI-stop-service
+#
+
+#
+## update the system ##
+#sudo apt-get update
+#sudo apt-get upgrade
+
+## stop service
+cd /usr/local/apachegui/ApacheGUI/bin
+./stop.sh
+
+echo " .... "
+echo " .... "
+echo " .... Service Stop .... "
+echo " .... "
+echo " .... "
