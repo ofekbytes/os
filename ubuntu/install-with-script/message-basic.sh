41b6eb67d9e8ac94c64e341e54e095cafb9b7efa
jron <yaron.kessler41@gmail.com>
2019-10-05 09:35:04 +0300

    test

---
+++ ubuntu/install-with-script/message-basic.sh
@@ -0,0 +1,14 @@
+echo "."
+echo "."
+echo "---[updating system files]---"
+echo "."
+echo "."
+sudo apt-get update
+echo "."
+echo "."
+echo "---[upgrading system files]---"
+echo "."
+echo "."
+sudo apt-get upgrade -
+echo "."
+echo "."
