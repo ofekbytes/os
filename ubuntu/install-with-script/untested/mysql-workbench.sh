caeb397d3fc54dfc0cb4a582549fbc5c875428a5
koskas <koskas@yahoo.com>
2020-03-20 10:45:29 +0200

    test

---
+++ ubuntu/install-with-script/untested/mysql-workbench.sh
@@ -0,0 +1,73 @@
+#  
+### mysql-workbench install ###
+#  
+
+
+echo "."
+echo "."
+echo "---[mysql-workbench - installation script]---"
+echo "."
+echo "."
+
+
+echo "."
+echo "."
+echo "---[updating system files]---"
+echo "."
+echo "."
+sudo apt-get update -y
+echo "."
+echo "."
+echo "---[upgradingecho "." system files]---"
+echo "."
+echo "."
+sudo apt-get upgrade -y
+echo "."
+echo "."
+
+
+echo "."
+echo "---[mysql-workbench install]---"
+echo "."
+echo "."
+sudo apt-get install mysql-workbench -y
+
+
+
+echo "."
+echo "."
+echo "---[updating system files]---"
+echo "."
+echo "."
+sudo apt-get update -y
+echo "."
+echo "."
+echo "---[upgradingecho "." system files]---"
+echo "."
+echo "."
+sudo apt-get upgrade -y
+echo "."
+echo "."
+
+
+echo "."
+echo "."
+echo "---[installation vestion]---"
+echo "."
+echo "."
+echo "mysql-workbench version: "
+mysql-workbench --version
+echo "."
+echo "."
+echo "."
+echo "."
+echo "---[mysql-workbench - installation completed.]---"
+echo "."
+echo "."
+
+
+
+
+
+
+
