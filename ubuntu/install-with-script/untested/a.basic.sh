928adabd6fce47c96585383a924a6a070729dbdc
jron <yaron.kessler41@gmail.com>
2020-04-10 09:31:40 +0300

    test

---
+++ ubuntu/install-with-script/untested/a.basic.sh
@@ -0,0 +1,70 @@
+#  
+### a.basic ###
+#  
+
+
+echo "."
+echo "."
+echo "---[a.basic - installation script]---"
+echo "."
+echo "."
+
+
+echo "."
+echo "."
+echo "---[updating system files]---"
+echo "."
+echo "."
+sudo apt-get update -y
+echo "."
+echo "."
+echo "---[upgradingecho "." system files]---"
+echo "."
+echo "."
+sudo apt-get upgrade -y
+echo "."
+echo "."
+
+
+#
+## net-tools installation (net command: ifconfig,...) ##
+echo "."
+echo "---[net-tools installation (net command: ifconfig,...)]---"
+echo "net-tools"
+echo "."
+echo "."
+sudo apt-get install net-tools -y
+
+
+#
+## vim installation ##
+echo "."
+echo "---[vim installation]---"
+echo "vim"
+echo "."
+echo "."
+sudo apt-get install vim -y
+
+
+echo "."
+echo "."
+echo "---[updating system files]---"
+echo "."
+echo "."
+sudo apt-get update -y
+echo "."
+echo "."
+echo "---[upgradingecho "." system files]---"
+echo "."
+echo "."
+sudo apt-get upgrade -y
+echo "."
+echo "."
+
+
+echo "."
+echo "."
+echo "."
+echo "."
+
+
---
+++ ubuntu/install-with-script/untested/net-tools.sh
@@ -0,0 +1,20 @@
+
+## a.basic  ##
+
+#
+## update the system ##
+sudo apt-get update
+sudo apt-get upgrade
+
+
+#
+## net-tools installation (net command: ifconfig,...)## 
+sudo apt-get install net-tools
+
+
+#
+## update the system ##
+sudo apt-get update
+sudo apt-get upgrade
+
+
