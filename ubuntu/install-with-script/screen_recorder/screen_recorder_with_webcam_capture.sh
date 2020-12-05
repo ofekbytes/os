530858e761e46b4c6216eddedced3da149f4a1c1
jron <yaron.kessler41@gmail.com>
2020-12-05 19:43:15 +0200

    screen recorder

---
+++ ubuntu/install-with-script/screen_recorder/screen_recorder_with_webcam_capture.sh
@@ -0,0 +1,17 @@
+
+## update the system##
+sudo apt-get update
+sudo apt-get upgrade
+
+
+sudo apt install vokoscreen
+sudo apt install snap
+sudo snap install shotcut --classic
+
+
+##update the system##
+sudo apt-get update
+sudo apt-get upgrade
+
+
+###The-End###
