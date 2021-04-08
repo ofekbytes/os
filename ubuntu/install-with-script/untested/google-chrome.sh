1bc481e2b4c824f2ade49e5a12e28a9ba205abe9
yaron <yaron.kessler41@gmail.com>
2021-04-08 05:54:40 +0300

    script

---
+++ ubuntu/install-with-script/untested/google-chrome.sh
@@ -0,0 +1,18 @@
+
+## apache2  ##
+
+#
+## update the system ##
+sudo apt-get update
+sudo apt-get upgrade
+
+#
+## download 
+## google-chrome-stable_current_amd64
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+
+#
+## install
+## google-chrome-stable_current_amd64
+sudo apt install ./google-chrome-stable_current_amd64.deb
