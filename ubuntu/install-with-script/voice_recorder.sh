74644b611421b86a4e3709ed60aec1f67faab911
ofekbytes <yaron.kessler41@gmail.com>
2024-08-30 06:53:27 +0300

    Create voice_recorder.sh

---
+++ ubuntu/install-with-script/voice_recorder.sh
@@ -0,0 +1,16 @@
+##----[ Beginning Of Script]----##
+
+
+##----[ System Update ]----##
+sudo apt-get update
+sudo apt-get upgrade
+
+##----[ Install ]----##
+sudo apt-get gnome-sound-recorder
+
+##----[ System Update ]----##
+sudo apt-get update
+sudo apt-get upgrade
+
+
+##----[ End Of Script]----##
